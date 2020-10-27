8c48f8ec75cadff185a1cc80c56afe9d1f01fb57
phuongyuki <dhaphuong123@gmail.com>
2020-10-27 11:36:24 +0700

    update files

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,12 +1,16 @@
 var http = require('http');
 var express = require('express');
 var nunjucks = require('nunjucks');
+//Import the mongoose module
+var mongoose = require('mongodb').MongoClient;
 var path = require('path');
+// const bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.json());
 app.use(express.static("express"));
 app.use(express.static(path.join(__dirname,'/public')))
+// app.use(bodyParser.urlencoded({extended: true}))
 
 nunjucks.configure(('express'),{
   express:app,
@@ -14,7 +18,6 @@ nunjucks.configure(('express'),{
   noCache:false,
   watch:true
 });
-
 
 // default URL for website
 app.get('/', function(req,res){
@@ -50,3 +53,25 @@ var server = http.createServer(app);
 var port = 3000;
 server.listen(port);
 console.debug('Server listening on port ' + port);
+
+var connectionString = 'mongodb+srv://todayuki:phuong2k11@web-demo-db.rhvu8.mongodb.net/web-demo-db?retryWrites=true&w=majority'
+
+//Set up default mongoose connection
+// var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(connectionString,{useUnifiedTopology: true
+  })
+  .then(client => {
+    console.log('Connect to Database')
+    const db = client.db('web-demo-db')
+    const quotesCollection = db.collection('quotes')
+    app.post('/register.html', (req, res)=>{
+      quotesCollection.insertOne(req.body)
+      .then (result =>{
+        res.redirect('/')
+      })
+      .catch(error => console.error(error))
+    })
+  })
+  .catch(error => console.error(error))
+
+
